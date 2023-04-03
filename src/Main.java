7bc996dc39a1cbd7fbf2d9cfc7382e7b244d3663
mouhsine82 <mboukhris6148@gmail.com>
2023-04-03 16:04:29 -0700

    The basics of solar system is ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem solar=new SolarSystem();
+        solar.moon="2";
+        solar.planets="2";
+        solar.sun="1";
+    }
+}
